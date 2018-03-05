d9a192ebb150a2d8b7690c1645ee78d69bf05e66
KristinaYukova <yukova.k@gmail.com>
2018-03-05 20:19:39 +0200

    charivna_mozaika

--- charivna/js/main.js
+++ charivna/js/main.js
@@ -228,4 +228,22 @@ jQuery(document).ready(function($){
   	map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
 });
 
+
+$('.owl-carousel').owlCarousel({
+
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
   
