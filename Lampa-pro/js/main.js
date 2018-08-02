d907644fed3717622cf5b39831ef6fe0b72b6d16
KristinaYukova <34583564+KristinaYukova@users.noreply.github.com>
2018-08-03 01:34:33 +0300

    lampa_pro

---
+++ Lampa-pro/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    autoHeight:true,
+    autoWidth:true,
+    items:4,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    center: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+});
