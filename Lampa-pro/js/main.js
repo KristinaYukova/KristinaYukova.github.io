cf9191ff98dd25fd27f8a917db5347af23b3c40c
KristinaYukova <34583564+KristinaYukova@users.noreply.github.com>
2018-08-06 19:24:29 +0300

    0.2

--- Lampa-pro/js/main.js
+++ Lampa-pro/js/main.js
@@ -11,7 +11,7 @@ $('.owl-carousel').owlCarousel({
     nav: false,
     dots: false,
     autoplay: true,
-    autoplayTimeout: 4000,
+    autoplayTimeout: 2000,
     autoplayHoverPause: true,
     center: true,
     responsive:{
